eabea48cae213b6646b61b49aea556bf2ef2be97
Dominik Moritz <domoritz@gmail.com>
2011-12-03 21:40:20 +0100

    latest date first

--- www/validitychecker/views.py
+++ www/validitychecker/views.py
@@ -51,7 +51,7 @@ def get_authors_and_articles_from_db(titles):
     param: title a list of strings    
     """
     authors = Author.objects.filter(articles__title__in=titles).distinct()
-    ret = [(author,Article.objects.filter(title__in=titles).filter(author__name=author.name)) for author in authors]
+    ret = [(author,Article.objects.filter(title__in=titles).filter(author__name=author.name).order_by('-publish_date')) for author in authors]
     #ret = Article.objects.filter(title__in=titles).values('author')
     #ret = Author.objects.filter(articles__title__in=titles).
     #print ret
