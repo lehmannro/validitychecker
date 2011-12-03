d578b3a154de0931dc7f48c535f656f396edc551
Robert Lehmann <mail@robertlehmann.de>
2011-12-03 14:25:20 +0100

    Added basic search workflow.

--- www/validitychecker/views.py
+++ www/validitychecker/views.py
@@ -1,6 +1,16 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+def results(request):
+    if 'q' in request.GET:
+        term = request.GET['q']
+        return render_to_response('results.html',
+                                  context_instance=RequestContext(request))
+    else:
+        return # 300 /index
+
 def index(request):
     return render_to_response('home.html',
-                              context_instance=RequestContext(request))
+                              context_instance=RequestContext(request, dict(
+                              target=reverse(results))))
