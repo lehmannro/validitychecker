c75706f2f93380f3414342ed1d2b574a966385a0
Robert Lehmann <mail@robertlehmann.de>
2011-12-03 23:25:47 +0100

    Bubblesort on the DOM isn't normal.  On RHoK it is.

--- overlay.user.js
+++ overlay.user.js
@@ -23,13 +23,9 @@ function getScore(author, title, callback) {
     });
 }
 
-function finish() {
-    // sort
-}
-
 var nodes = document.getElementsByClassName("gs_r");
 var completed = 0;
-for (i = 0; i < nodes.length; i++) {
+for (var i = 0; i < nodes.length; i++) {
     (function(){
     var article = nodes[i];
     var meta = article.getElementsByClassName("gs_a")[0];
@@ -38,15 +34,25 @@ for (i = 0; i < nodes.length; i++) {
     var title = article.getElementsByTagName("h3")[0].textContent;
     getScore(author, title, function(response) {
         completed += 1;
+        var score = parseInt(response.responseText);
+        article.setAttribute("score", score);
         if (nodes.length == completed) {
             finish();
-        }
-        var score = parseInt(response.responseText);
-        if (score < 20) {
-            article.style.backgroundColor = "red";
         }
         meta.appendChild(
             document.createTextNode(" - Credibility: " + score + "% "));
     });
     })();
+}
+
+function finish() {
+    for (var i = 0; i < nodes.length; i++) {
+        for (var j = 0; j < nodes.length-1; j++) {
+            cur = nodes[j]; next = nodes[j+1];
+            if (cur.getAttribute("score") < next.getAttribute("score")) {
+                next.parentElement.removeChild(next);
+                cur.parentElement.insertBefore(next, cur);
+            }
+        }
+    }
 }
