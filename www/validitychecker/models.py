b943555af845c0da2efb5cd3686dbbe4f3ac0ef6
Dominik Moritz <domoritz@gmail.com>
2011-12-03 13:57:06 +0100

    started models

--- www/validitychecker/models.py
+++ www/validitychecker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    ranking = models.IntegerField()
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    abstract = models.TextField()
+    release_date = models.DateField()
+
