68052441ee9687fecc680a0331c37ca67b5c19af
Dominik Moritz <domoritz@gmail.com>
2011-12-03 14:06:41 +0100

    models fixed

--- www/validitychecker/models.py
+++ www/validitychecker/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-class Author(models.Model):
-    articles = models.ManyToManyField(Articles, verbose_name="list of articles")
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    ranking = models.IntegerField()
-
 class Article(models.Model):
     title = models.CharField(max_length=255)
     abstract = models.TextField()
     release_date = models.DateField()
 
+class Author(models.Model):
+    articles = models.ManyToManyField(Article, verbose_name="list of articles")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    ranking = models.IntegerField()
