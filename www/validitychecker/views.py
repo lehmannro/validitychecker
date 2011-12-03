29301aa4ff720a4d5f328c0bbf854d6e51ce4430
Dominik Moritz <domoritz@gmail.com>
2011-12-03 17:02:43 +0100

    better query for increment

--- www/validitychecker/views.py
+++ www/validitychecker/views.py
@@ -2,13 +2,16 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from validitychecker.models import Query
+from django.db.models import F
 
 def results(request):
     if 'q' in request.GET:
         query = request.GET['q']
 
         #save query to db
-        qobj, created = Query.objects.get_or_create(query=query, defaults={'query':query, 'number':0}).update(views=F('views')+1)
+        qobj, created = Query.objects.get_or_create(query=query, defaults={'query':query, 'number':0})
+        qobj.number = F('number') + 1
+        qobj.save()
 
         resultset = get_results(query)
         return render_to_response('results.html',
