679dd09fd0d85216c2fbbdbf9963edddb3e2998a
Dominik Moritz <domoritz@gmail.com>
2011-12-03 14:17:05 +0100

    release -> publish

--- www/validitychecker/models.py
+++ www/validitychecker/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-    articles = models.ManyToManyField("Article", verbose_name="list of articles")
+    articles = models.ManyToManyField('Article', verbose_name="list of articles")
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     ranking = models.IntegerField()
@@ -9,5 +9,5 @@ class Author(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=255)
     abstract = models.TextField()
-    release_date = models.DateField()
+    publish_date = models.DateField('date published')
 
