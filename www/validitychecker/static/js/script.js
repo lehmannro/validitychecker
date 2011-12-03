4bc053411d985c3ce8bb49f687791ba003dbd3dc
Norman Rzepka <code@normanrzepka.de>
2011-12-03 15:34:52 -0800

    code cleaning

--- www/validitychecker/static/js/script.js
+++ www/validitychecker/static/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         //$('.mask').show();
     });
 
-    $("#popular").tagcloud({type:"list",sizemin:16, sizemax:30, colormin:"aaa",colormax:"888"}).find("li");
+    $(".home .popular").tagcloud({type:"list", sizemin:16, sizemax:30, colormin:"aaa", colormax:"888"}).find("li");
 });
 
 
