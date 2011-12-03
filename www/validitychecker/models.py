a3693b8fdc7cbcac5cd22995cade44a3dec6621d
Dominik Moritz <domoritz@gmail.com>
2011-12-03 14:07:56 +0100

    model author above article

--- www/validitychecker/models.py
+++ www/validitychecker/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+class Author(models.Model):
+    articles = models.ManyToManyField("Article", verbose_name="list of articles")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    ranking = models.IntegerField()
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     abstract = models.TextField()
     release_date = models.DateField()
 
-class Author(models.Model):
-    articles = models.ManyToManyField(Article, verbose_name="list of articles")
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    ranking = models.IntegerField()
