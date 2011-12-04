05f869002b00c46021954e63f1ad38a059832aea
Norman Rzepka <code@normanrzepka.de>
2011-12-03 16:41:00 -0800

    blabla

--- www/validitychecker/static/js/script.js
+++ www/validitychecker/static/js/script.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function() {
 
-    $(".home .popular").tagcloud({type:"list", sizemin:16, sizemax:30, colormin:"aaa", colormax:"888"}).find("li");
+    $(".home .popular ul").tagcloud({type:"list", sizemin:16, sizemax:30, colormin:"aaa", colormax:"888"}).find("li");
 
 });
 
